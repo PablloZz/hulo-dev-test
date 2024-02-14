fb9f341f86eaff75efeaeedc1a8e2d6c85ce35b8
Pavlo Zalutskiy <kyrogane1@gmail.com>
2024-02-14 15:26:23 +0200

    fix: Add missing code for slide switching

--- src/html/slider/slider.ts
+++ src/html/slider/slider.ts
@@ -1,8 +1,50 @@
 import { vimeoApi } from "../../packages/vimeo/vimeo.js";
 import { createElement } from "../../libs/helpers/helpers.js";
-import { SLIDES_COUNT } from "./libs/constants/constants.js";
+import {
+  START_SLIDER_POSITION,
+  SLIDE_WIDTH,
+  END_SLIDER_POSITION,
+  SLIDES_COUNT,
+} from "./libs/constants/constants.js";
+import { getSliderPosition } from "./libs/helpers/helpers.js";
 
 const slider = document.querySelector(".slider") as HTMLDivElement;
+const nextSlideButton = document.querySelector(
+  ".next-slide"
+) as HTMLButtonElement;
+const previousSlideButton = document.querySelector(
+  ".previous-slide"
+) as HTMLButtonElement;
+
+function getPreviousSlide() {
+  const sliderPosition = getSliderPosition(slider);
+  const isStartPosition = sliderPosition === START_SLIDER_POSITION;
+  let newSliderPosition: string;
+
+  if (isStartPosition) {
+    newSliderPosition = `${END_SLIDER_POSITION}px`;
+    slider.style.setProperty("--slider-translate", newSliderPosition);
+    return;
+  }
+
+  newSliderPosition = `${sliderPosition + SLIDE_WIDTH}px`;
+  slider.style.setProperty("--slider-translate", newSliderPosition);
+}
+
+function getNextSlide() {
+  const sliderPosition = getSliderPosition(slider);
+  const isEndPosition = sliderPosition <= END_SLIDER_POSITION;
+  let newSliderPosition: string;
+
+  if (isEndPosition) {
+    newSliderPosition = `${START_SLIDER_POSITION}px`;
+    slider.style.setProperty("--slider-translate", newSliderPosition);
+    return;
+  }
+
+  newSliderPosition = `${sliderPosition - SLIDE_WIDTH}px`;
+  slider.style.setProperty("--slider-translate", newSliderPosition);
+}
 
 async function initSlidePreview(slideNumber: number) {
   const slideId = `slide-${slideNumber}`;
@@ -33,6 +75,8 @@ function initSlidesPreview() {
 
 function initSlider() {
   initSlidesPreview();
+  previousSlideButton.addEventListener("click", getPreviousSlide);
+  nextSlideButton.addEventListener("click", getNextSlide);
 }
 
 export { initSlider };
