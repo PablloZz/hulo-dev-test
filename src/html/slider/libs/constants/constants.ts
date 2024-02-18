15d435a9616147d99e4767fb33365d01cebb879e
Pavlo Zalutskiy <kyrogane1@gmail.com>
2024-02-18 17:41:02 +0200

    fix: Remove no longer needed constants

--- src/html/slider/libs/constants/constants.ts
+++ src/html/slider/libs/constants/constants.ts
@@ -1,4 +1,2 @@
-export { SLIDE_WIDTH } from "./slide-width.constant.js";
 export { SLIDES_COUNT } from "./slides-count.constant.js";
-export { END_SLIDER_POSITION } from "./end-slider-position.constant.js";
 export { START_SLIDER_POSITION } from "./start-slider-position.constant.js";
--- src/html/slider/libs/constants/slide-width.constant.ts
+++
@@ -1,3 +0,0 @@
-const SLIDE_WIDTH = 250;
-
-export { SLIDE_WIDTH };
